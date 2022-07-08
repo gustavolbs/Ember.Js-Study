6ed8bf8f48da0e8e5eb8fab07d8962b2fffd5d6c
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-07-08 15:21:48 -0300

    chapter 9 (tests)

---
+++ tests/integration/components/star-rating-test.js
@@ -0,0 +1,79 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render, click } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import EmberObject from "@ember/object";
+
+module("Integration | Component | star-rating", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("Renders the full and empty stars correctly", async function (assert) {
+    this.set("rating", 4);
+    this.set("maxRating", 5);
+
+    await render(
+      hbs`<StarRating @rating={{this.rating}} @maxRating={{this.maxRating}} />`
+    );
+
+    assert
+      .dom(".fa-star")
+      .exists(
+        { count: 4 },
+        "The right amount of full stars is rendered after changing maxRating"
+      );
+    assert
+      .dom(".fa-star-o")
+      .exists(
+        { count: 1 },
+        "The right amount of empty stars is rendered after changing maxRating"
+      );
+
+    this.set("maxRating", 10);
+
+    assert
+      .dom(".fa-star")
+      .exists(
+        { count: 4 },
+        "The right amount of full stars is rendered after changing maxRating"
+      );
+    assert
+      .dom(".fa-star-o")
+      .exists(
+        { count: 6 },
+        "The right amount of empty stars is rendered after changing maxRating"
+      );
+
+    this.set("rating", 2);
+
+    assert
+      .dom(".fa-star")
+      .exists(
+        { count: 2 },
+        "The right amount of full stars is rendered after changing maxRating"
+      );
+    assert
+      .dom(".fa-star-o")
+      .exists(
+        { count: 8 },
+        "The right amount of empty stars is rendered after changing maxRating"
+      );
+  });
+
+  test("The setRating action", async function (assert) {
+    this.set("song", EmberObject.create({ rating: 3 }));
+    this.set("updateRating", function (song, rating) {
+      song.set("rating", rating);
+    });
+
+    await render(
+      hbs`<StarRating @rating={{this.rating}} @onClick={{fn this.updateRating song}} />`
+    );
+    await click("[data-test-rr=star-rating-5]");
+
+    assert.equal(
+      this.get("song.rating"),
+      5,
+      "The click star's rating is correctly sent"
+    );
+  });
+});
